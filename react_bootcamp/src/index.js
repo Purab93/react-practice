e07c5ba7c2eaae7b4b1c24d0e95e5684c4863e1f
Purab Shah <purab@Purabs-MacBook-Air.local>
2018-10-22 01:17:03 +0530

    add base files

---
+++ react_bootcamp/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Components
+import Header from './components/header';
+ 
+const App = () =>{
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+ }
+
+ReactDOM.render(<App/>,document.querySelector('#root'));
