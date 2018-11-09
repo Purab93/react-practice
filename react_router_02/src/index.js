b59f0c65b08648b1f33ec7f235e84d6fcb1949d8
Purabsh93 <purab.shah@airpay.co.in>
2018-11-09 18:17:50 +0530

    add basic routers code

--- react_router_02/src/index.js
+++ react_router_02/src/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {BrowserRouter, Route, NavLink, Switch } from 'react-router-dom';
+
+// Components
+import Home from './components/home';
+import Posts from './components/posts';
+import Profile from './components/profiles';
+import PostItem from './components/post_item';
+
+const App = () =>{
+    return (<div>
+        <BrowserRouter>
+            <div>
+                <header>
+                    <NavLink to='/'>Home</NavLink><br />
+                    <NavLink 
+                        to='/posts'
+                        activeStyle={{color:'red'}}
+                        activeClassName="selected "
+                    >Post</NavLink><br />
+                    <NavLink to='/profile'>Profile</NavLink><br />
+                    <hr/>
+                </header>
+                <Switch>
+                    <Route path="/posts/:id/:username" component={PostItem} />
+                    <Route path="/profile" component={Profile} />
+                    <Route path="/posts" component={Posts} />
+                    <Route path="/" exact component={Home} />
+                    <Route component={Posts}/>
+                </Switch>
+            </div>
+        </BrowserRouter>
+    </div>)
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
