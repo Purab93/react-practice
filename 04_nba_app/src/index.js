85870031792c1795076dd163fdfc679ecad1681a
Purab Shah <Purab@Purabs-MacBook-Pro.local>
2018-11-26 17:33:02 +0530

    before eject

--- 04_nba_app/src/index.js
+++ 04_nba_app/src/index.js
@@ -7,9 +7,7 @@ import Routes from './routes'
 const App = () => {
     return (
         <BrowserRouter>
-            <Routes>
-
-            </Routes>
+            <Routes />
         </BrowserRouter>
     );
 }
--- 04_nba_app/src/routes.js
+++ 04_nba_app/src/routes.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 
-import Home from './components/home/home'
+import Home from './components/home/home';
+import Layout from "./hoc/layout/layout";
 
 class Routes extends Component{
     render() {
         return (
-            <Switch>
-                <Route path="/" exact component={Home } />
-            </Switch>
+            <Layout>
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                </Switch>
+            </Layout>
         )
     }
 }
